aa4b36d6b308303dc30499cbac29f9a48e75be1d
p1nkf1oyd <ubpavelub@mail.ru>
2022-12-14 18:00:15 +0300

    1

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ btnClose.addEventListener('click', () => {
     menu.classList.add('hidden');
     document.body.style.overflow='scroll'
 })
+
+const accordion = document.getElementsByClassName('contentBx');
+
+        for (i = 0; i<accordion.length; i++){
+            accordion[i].addEventListener('click', function(){
+                this.classList.toggle('active')
+            })
+        }
