64fec1a1066cca8ea95671f88f7475ed8a55179f
p1nkf1oyd <ubpavelub@mail.ru>
2022-12-14 22:48:30 +0300

    1

--- index.js
+++ index.js
@@ -9,32 +9,10 @@ btnClose.addEventListener('click', () => {
     menu.classList.add('hidden');
     document.body.style.overflow='scroll'
 })
-
 const accordion = document.getElementsByClassName('contentBx');
 
-        for (i = 0; i<accordion.length; i++){
-            accordion[i].addEventListener('click', function(){
-                this.classList.toggle('active')
-            })
-        }
-const btn1 = document.querySelector('че нибудь') 
-
-const btn2 = document.querySelector('че нибудь другое') 
-
-btn1.addEventListener('click', () => {
-   modal('класс модалки') 
-}) 
-
-btn2.addEventListener('click', () => {
-   modal('класс другой мрдалки') 
-}) 
-
-// функция скпытия/показа модалки
-function modal(selector) {
-// селектим модалку
-    const popup = document.querySelector(selector) 
-   // меняем ее состояние  if(popup.classList.contains('hidden') {
-   popup.classList.remove('hidden') 
-} else {
-   popup.classList.add('hidden')  
-} 
+      for (i = 0; i<accordion.length; i++){
+        accordion[i].addEventListener('click', function(){
+            this.classList.toggle('active')
+        })
+      }
