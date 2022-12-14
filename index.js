afe6bd052f83287c5ec9f6d5831922e088f17a61
p1nkf1oyd <ubpavelub@mail.ru>
2022-12-14 22:35:47 +0300

    1

--- index.js
+++ index.js
@@ -17,3 +17,24 @@ const accordion = document.getElementsByClassName('contentBx');
                 this.classList.toggle('active')
             })
         }
+const btn1 = document.querySelector('че нибудь') 
+
+const btn2 = document.querySelector('че нибудь другое') 
+
+btn1.addEventListener('click', () => {
+   modal('класс модалки') 
+}) 
+
+btn2.addEventListener('click', () => {
+   modal('класс другой мрдалки') 
+}) 
+
+// функция скпытия/показа модалки
+function modal(selector) {
+// селектим модалку
+    const popup = document.querySelector(selector) 
+   // меняем ее состояние  if(popup.classList.contains('hidden') {
+   popup.classList.remove('hidden') 
+} else {
+   popup.classList.add('hidden')  
+} 
